eacff584fc10d04bf0cfea039f3acaf379d2224c
Priyansh Nama <54625644+priyanshnama@users.noreply.github.com>
2020-07-13 18:31:50 +0530

    ResultActivity fully optimized.

--- app/src/main/java/com/priyanshnama/herbarium/ResultActivity.java
+++ app/src/main/java/com/priyanshnama/herbarium/ResultActivity.java
@@ -4,21 +4,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-
 import java.util.Objects;
 
 public class ResultActivity extends AppCompatActivity {
-    private RecyclerView recyclerView;
-    private TextView sample;
-    private String Sample_Name = "sample";
-    private String name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,30 +18,25 @@ public class ResultActivity extends AppCompatActivity {
         setContentView(R.layout.activity_result);
         Objects.requireNonNull(getSupportActionBar()).setTitle("Best Predictions");
 
-        recyclerView = findViewById(R.id.recyclerView);
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setHasFixedSize(true);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
         recyclerView.setLayoutManager(linearLayoutManager);
         recyclerView.setVisibility(View.INVISIBLE);
 
-        sample = findViewById(R.id.sample);
-        sample.setText("" + Sample_Name  + "\n Probability : 98.5%");
-        sample.setOnClickListener(v -> show(Sample_Name));
+        TextView sample = findViewById(R.id.sample);
+        sample.setText(String.format("%s\n Probability : 98.5%%", "sample"));
+        sample.setOnClickListener(v -> show());
     }
 
-    private void show(String sample_name) {
+    private void show() {
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         db.collection("Leaves")
                 .document("Sample")
                 .get()
                 .addOnCompleteListener(task -> {
                     if (task.isSuccessful()) {
-                        DocumentSnapshot data = task.getResult();
-                        assert data != null;
-                        name = (String)Objects.requireNonNull(data.getData()).get("Name");
-                        Intent intent = new Intent(ResultActivity.this, InfoActivity.class);
-                        intent.putExtra("Name",name);
-                        startActivity(intent);
+                        startActivity(new Intent(ResultActivity.this, InfoActivity.class).putExtra("Name", Objects.requireNonNull(Objects.requireNonNull(Objects.requireNonNull(task.getResult()).getData()).get("Name")).toString()));
                     }
                 });
     }
