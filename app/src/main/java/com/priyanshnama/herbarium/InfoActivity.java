1274b3f6ec15e8db2d3a224720c3a1bdf6889f31
Priyansh Nama <54625644+priyanshnama@users.noreply.github.com>
2020-07-13 18:20:07 +0530

    InfoActivity fully optimized.

--- app/src/main/java/com/priyanshnama/herbarium/InfoActivity.java
+++ app/src/main/java/com/priyanshnama/herbarium/InfoActivity.java
@@ -3,20 +3,15 @@ package com.priyanshnama.herbarium;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-
 import java.util.Objects;
 
 public class InfoActivity extends AppCompatActivity {
-    private FloatingActionButton wiki;
-    private String link, name, botanical, family, medicine, parts;
+    private String link, name;
     private TextView Name, Botanical, Medicine, Family, Parts;
 
     @Override
@@ -26,17 +21,15 @@ public class InfoActivity extends AppCompatActivity {
         Objects.requireNonNull(getSupportActionBar()).hide();
         name = Objects.requireNonNull(getIntent().getExtras()).getString("Name");
 
-        wiki = findViewById(R.id.wiki);
+        FloatingActionButton wiki = findViewById(R.id.wiki);
         Name = findViewById(R.id.name);
         Medicine = findViewById(R.id.medicine);
         Family = findViewById(R.id.family);
         Parts = findViewById(R.id.parts);
         Botanical = findViewById(R.id.botanical);
 
-        Log.d("D","name is "+ name);
         get_all_data();
-
-        wiki.setOnClickListener(v -> open_wiki(link));
+        wiki.setOnClickListener(v -> startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(link))));
     }
 
     private void get_all_data() {
@@ -49,23 +42,12 @@ public class InfoActivity extends AppCompatActivity {
                         DocumentSnapshot data = task.getResult();
                         assert data != null;
                         link = (String) Objects.requireNonNull(data.getData()).get("Wiki Link");
-                        botanical = (String) Objects.requireNonNull(data.getData()).get("Botanical Name");
-                        family = (String) Objects.requireNonNull(data.getData()).get("Family");
-                        medicine = (String) Objects.requireNonNull(data.getData()).get("Medical Use");
-                        parts = (String) Objects.requireNonNull(data.getData()).get("Parts Used");
-
-                        Name.setText(Name.getText() + "\n" + name);
-                        Family.setText(Family.getText() + "\n" + family);
-                        Medicine.setText(Medicine.getText() + "\n" + medicine);
-                        Botanical.setText(Botanical.getText() + "\n" + botanical);
-                        Parts.setText(Parts.getText() + "\n" + parts);
-                        Log.d("URI","Url is "+ link);
+                        Name.setText(String.format("%s\n%s", getString(R.string.name), name));
+                        Family.setText(String.format("%s\n%s", getString(R.string.family), Objects.requireNonNull(data.getData()).get("Family")));
+                        Medicine.setText(String.format("%s\n%s", getString(R.string.medicine), Objects.requireNonNull(data.getData()).get("Medical Use")));
+                        Botanical.setText(String.format("%s\n%s", getString(R.string.botanical_name), Objects.requireNonNull(data.getData()).get("Botanical Name")));
+                        Parts.setText(String.format("%s\n%s", getString(R.string.parts_used), Objects.requireNonNull(data.getData()).get("Parts Used")));
                     }
                 });
-    }
-
-    private void open_wiki(String link) {
-        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(link));
-        startActivity(browserIntent);
     }
 }
